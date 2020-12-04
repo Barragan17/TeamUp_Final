f83a5e790d98707d2151b138124c0f527426a988
Barragan17 <50490377+Barragan17@users.noreply.github.com>
2020-12-04 16:24:19 +0700

    Add files via upload

--- lib/screen/project_profile.dart
+++ lib/screen/project_profile.dart
@@ -16,11 +16,19 @@ class _ProjectProfileState extends State<ProjectProfile> {
               width: 450,
               height: 350,
               child: Container(
+<<<<<<< HEAD
+                color: Colors.blue,
+              )),
+          Positioned(
+              top: 90.0,
+              left: 45.0,
+=======
                 color: Colors.orange,
               )),
           Positioned(
               top: 90.0,
               left: 78.0,
+>>>>>>> 809b385eb804cb6063a39b93347fce6b5ea3cca3
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: <Widget>[
@@ -31,7 +39,11 @@ class _ProjectProfileState extends State<ProjectProfile> {
                         height: ScreenUtil().setWidth(400),
                         fit: BoxFit.cover,
                         image: new AssetImage(
+<<<<<<< HEAD
+                            'assets/images/ad7e551b033b6da8f428df5b159651c7a8506006.png')),
+=======
                             'assets/images/ad7e551b033b6da8f428df5b159651c7a8506006.PNG')),
+>>>>>>> 809b385eb804cb6063a39b93347fce6b5ea3cca3
                   ),
                   new SizedBox(
                     height: ScreenUtil().setHeight(40.0),
@@ -74,7 +86,11 @@ class _ProjectProfileState extends State<ProjectProfile> {
                   Padding(
                     padding: const EdgeInsets.all(15.0),
                     child: Text(
+<<<<<<< HEAD
+                      "Project",
+=======
                       "Profile",
+>>>>>>> 809b385eb804cb6063a39b93347fce6b5ea3cca3
                       style: TextStyle(
                           fontFamily: 'Montserrat',
                           fontWeight: FontWeight.bold,
@@ -89,6 +105,106 @@ class _ProjectProfileState extends State<ProjectProfile> {
             left: 20.0,
             child: Column(
               children: [
+<<<<<<< HEAD
+                Container(
+                  margin: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 215, 0),
+                        child: Text(
+                          "Deadline",
+                          style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 215, 0),
+                        child: Text(
+                          "Add Date",
+                          style:
+                              TextStyle(fontFamily: 'Montserrat', fontSize: 16),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(10, 0, 150, 0),
+                        child: Text(
+                          "About The Project",
+                          style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(10, 0, 170, 0),
+                        child: Text(
+                          "Add Description",
+                          style:
+                              TextStyle(fontFamily: 'Montserrat', fontSize: 16),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 250, 0),
+                        child: Text(
+                          "Tags",
+                          style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 215, 0),
+                        child: Text(
+                          "Add Tags",
+                          style:
+                              TextStyle(fontFamily: 'Montserrat', fontSize: 16),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 170, 0),
+                        child: Text(
+                          "Skilss Needed",
+                          style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 210, 0),
+                        child: Text(
+                          "Add Skills",
+                          style:
+                              TextStyle(fontFamily: 'Montserrat', fontSize: 16),
+                        ),
+                      ),
+                    ],
+=======
                 Row(
                   children: [
                     Text(
@@ -113,6 +229,7 @@ class _ProjectProfileState extends State<ProjectProfile> {
                   child: Text(
                     "- Virtual Hack UGM",
                     style: TextStyle(fontFamily: 'Montserrat', fontSize: 16),
+>>>>>>> 809b385eb804cb6063a39b93347fce6b5ea3cca3
                   ),
                 ),
               ],
